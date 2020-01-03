fbb0fff3d7bd44b9d543bed7558d8799b4ef8138
Kimaru thagana <macbuntu@localhost.localdomain>
2020-01-03 21:17:52 +0300

    view data obtained through requests.get()

--- scrap_table.py
+++ scrap_table.py
@@ -3,3 +3,6 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 url_to_web = 'https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_by_continent_(data_file)'
+# view the data from requests
+data = requests.get(url_to_web)
+print(f'Data from provided url\n {data.content}')
