9834bdd2bd167deb1eddbcab0b12eeb02c1673c6
Kimaru thagana <macbuntu@localhost.localdomain>
2020-01-03 21:04:06 +0300

    add necessary libraries and practice link

---
+++ scrap_table.py
@@ -0,0 +1,5 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+url_to_web = 'https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_by_continent_(data_file)'
