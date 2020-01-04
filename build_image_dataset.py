343c2e064a90cfa70f52615b2653878988167bbb
Kimaru thagana <macbuntu@localhost.localdomain>
2020-01-04 18:19:59 +0300

    add calling code

--- build_image_dataset.py
+++ build_image_dataset.py
@@ -1,14 +1,16 @@
 import os
 from fetch_image_urls import download, fetch_img_urls
 from selenium import webdriver
-def search_and_download(query ,driver_path, target_path='./images' ,number_images=5):
+def search_and_download(query ,driver_path, target_path='/IMG_DATASET' ,number_images=5):
     target_folder = os.path.join(target_path ,'_'.join(query.lower().split(' ')))
 
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
 
     with webdriver.Chrome(executable_path=driver_path) as wd:
-        res = fetch_img_urls(query, number_images, wd=wd, interval=0.5)
+        response = fetch_img_urls(query, number_images, wd=wd, interval=0.5)
 
-    for elem in res:
-        download(target_folder ,elem)
+    for url in response:
+        download(target_folder ,url)
+selenium_driver_path = ''
+search_and_download('bus', driver_path = selenium_driver_path)
