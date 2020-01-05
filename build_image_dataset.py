7e2835b1b4aa32dd375a94dd92a65adb06255edd
Kimaru thagana <macbuntu@192.168.1.7>
2020-01-05 17:53:40 +0300

    final testing

--- build_image_dataset.py
+++ build_image_dataset.py
@@ -1,7 +1,7 @@
 import os
 from fetch_image_urls import download, fetch_img_urls
 from selenium import webdriver
-def search_and_download(query ,driver_path, target_path='/IMG_DATASET' ,number_images=5):
+def search_and_download(query ,driver_path, target_path='IMG_DATASET' ,number_images=5):
     target_folder = os.path.join(target_path ,'_'.join(query.lower().split(' ')))
 
     if not os.path.exists(target_folder):
@@ -12,5 +12,5 @@ def search_and_download(query ,driver_path, target_path='/IMG_DATASET' ,number_i
 
     for url in response:
         download(target_folder ,url)
-selenium_driver_path = ''
+selenium_driver_path = '/home/macbuntu/Desktop/chromedriver_linux64/chromedriver'
 search_and_download('bus', driver_path = selenium_driver_path)
