5eb3b83173929f4311b01f119ee50bd0589e95b1
Kimaru thagana <macbuntu@localhost.localdomain>
2020-02-16 18:05:40 +0300

    get data from web

--- yahoo_commodities.py
+++ yahoo_commodities.py
@@ -15,3 +15,20 @@ url = "https://in.finance.yahoo.com/commodities"
 r= requests.get(url)
 data=r.text
 soup=BeautifulSoup(data)
+
+for row in soup.find_all('tbody'):
+    for srow in row.find_all('tr'):
+        for name in srow.find_all('td', attrs={'class':'data-col1'}):
+            names.append(name.text)
+        for price in srow.find_all('td', attrs={'class':'data-col2'}):
+            prices.append(price.text)
+        for time in srow.find_all('td', attrs={'class':'data-col3'}):
+            marketTimes.append(time.text)
+        for change in srow.find_all('td', attrs={'class':'data-col4'}):
+            changes.append(change.text)
+        for percentChange in srow.find_all('td', attrs={'class':'data-col5'}):
+            percentChanges.append(percentChange.text)
+        for volume in srow.find_all('td', attrs={'class':'data-col6'}):
+            totalVolumes.append(volume.text)
+        for openInterest in srow.find_all('td', attrs={'class':'data-col7'}):
+            openInterests.append(openInterest.text)
