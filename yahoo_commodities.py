fc8488d70b0d453e4809d4b5bef9d9701fb8d300
Kimaru thagana <macbuntu@localhost.localdomain>
2020-02-16 18:02:42 +0300

    create soup object

--- yahoo_commodities.py
+++ yahoo_commodities.py
@@ -1,3 +1,17 @@
-import requests from bs4
-import BeautifulSoup import csv
+import requests, csv
+from bs4 import BeautifulSoup
 import pandas as pd
+# variables for columns
+prices=[]
+names=[]
+changes=[]
+percentChanges=[]
+marketCaps=[]
+marketTimes=[]
+totalVolumes=[]
+openInterests=[]
+
+url = "https://in.finance.yahoo.com/commodities"
+r= requests.get(url)
+data=r.text
+soup=BeautifulSoup(data)
